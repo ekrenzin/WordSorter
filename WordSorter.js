1d3e968aba13f5562b8e80fe6408216fd52bff9f
Ean Krenzin <ean@eankrenzin.com>
2021-04-04 10:04:29 -0700

    Add files via upload

--- WordSorter.js
+++ WordSorter.js
@@ -6,77 +6,98 @@ class Word {
     */
     constructor(spelling, phonetic) {
         this.spelling = spelling
+        this.phonetic = this.toPhonetic()
+    }
+
+    toPhonetic() {
+        let chars = []
+        const spelling = this.spelling
+        const getLetterPhonetic = (char) => {
+            for (let letter of alphabet) if (char === letter.spelling) return letter.phonetic
+        }
+
+        for (let char of spelling) {
+            chars.push(getLetterPhonetic(char))
+        }
+
+        return chars.join('')
+    }
+}
+
+class Letter {
+    constructor(spelling, phonetic) {
+        this.spelling = spelling
         this.phonetic = phonetic
     }
 }
 
 const alphabet = [
-    new Word('a', "a"),
-    new Word('b', "bee"),
-    new Word('c', "cee"),
-    new Word('d', "dee"),
-    new Word('e', "e"),
-    new Word('f', "ef"),
-    new Word('g', "gee"),
-    new Word('h', "aitch"),
-    new Word('i', "i"),
-    new Word('j', "jay"),
-    new Word('k', "kay"),
-    new Word('l', "el"),
-    new Word('m', "em"),
-    new Word('n', "en"),
-    new Word('o', "o"),
-    new Word('p', "pee"),
-    new Word('q', "cue"),
-    new Word('r', "ar"),
-    new Word('s', "es"),
-    new Word('t', "tee"),
-    new Word('u', "u"),
-    new Word('v', "vee"),
-    new Word('w', "doubleu"),
-    new Word('x', "ex"),
-    new Word('y', "wy"),
-    new Word('z', "zee"),
+    new Letter('a', "a"),
+    new Letter('b', "bee"),
+    new Letter('c', "cee"),
+    new Letter('d', "dee"),
+    new Letter('e', "e"),
+    new Letter('f', "ef"),
+    new Letter('g', "gee"),
+    new Letter('h', "aitch"),
+    new Letter('i', "i"),
+    new Letter('j', "jay"),
+    new Letter('k', "kay"),
+    new Letter('l', "el"),
+    new Letter('m', "em"),
+    new Letter('n', "en"),
+    new Letter('o', "o"),
+    new Letter('p', "pee"),
+    new Letter('q', "cue"),
+    new Letter('r', "ar"),
+    new Letter('s', "es"),
+    new Letter('t', "tee"),
+    new Letter('u', "u"),
+    new Letter('v', "vee"),
+    new Letter('w', "doubleu"),
+    new Letter('x', "ex"),
+    new Letter('y', "wy"),
+    new Letter('z', "zee"),
 ]
 
+//let's assume these come from a db
 const words = [
-    new Word('apple', "see"),
-    new Word('brown', "see"),
-    new Word('bread', "see"),
-    new Word('bear', "see"),
-    new Word('built', "see"),
-    new Word('even weed', "even weed"),
+    new Word(spelling = 'apple'),
+    new Word(spelling = 'brown'),
+    new Word(spelling = 'bread'),
+    new Word(spelling = 'bear'),
+    new Word(spelling = 'built'),
 ]
 
-const arbitrayKey = [
-    'o','a', 'r', 'h', 'b', 'c', 'q', 'd',
+const inputAlphabeticalOrder = [
+    'o', 'a', 'r', 'h', 'b', 'c', 'q', 'd',
     'w', 'y', 'e', 'f', 'l', 'm', 'n',
-    's', 'x', 'g', 'i', 'j', 'k', 
+    's', 'x', 'g', 'i', 'j', 'k',
     'p', 't', 'u', 'v', 'z'
 ]
 
-const sortTypes = ['default', 'phonetic', 'arbitraryKeySort']
+const sort = (alphabeticalOrder) => {
+    const sortTypes = ['default', 'phonetic', 'arbitraryKeySort']
 
-const sortWords = (method) => {
-    let output = []
-    if (method == 'default') output = defaultSort()
-    if (method == 'phonetic') output = phoneticSort()
-    if (method == 'arbitraryKeySort') output = arbitraryKeySort()
-    return output
-}
+    const sortWords = (method) => {
+        if (method == 'default') return words.sort(defaultSort)
+        if (method == 'phonetic') return words.sort(phoneticSort)
+        if (method == 'arbitraryKeySort') return words.sort((a, b) => {
+            return (arbitraryKeySort({ a, b, property: 'spelling' }))
+        })
+    }
 
-const defaultSort = () => {
-    return words.sort((a, b) => (a.spelling > b.spelling) ? 1 : -1)
-}
+    const defaultSort = () => (a, b) => (a.spelling > b.spelling) ? 1 : -1
 
-const phoneticSort = () => {
-    return words.sort((a, b) => (a.phonetic > b.phonetic) ? 1 : -1)
-}
 
-const arbitraryKeySort = () => {
-    return words.sort((a, b) => {
-        let relativeIdxA = arbitrayKey.indexOf(a.spelling[0])
-        let relativeIdxB = arbitrayKey.indexOf(b.spelling[0])
+    const phoneticSort = (a, b) => {
+        return arbitraryKeySort({ a, b, property: 'phonetic' })
+    }
+
+    const arbitraryKeySort = (data) => {
+        const { a, b, property } = data
+        let relativeIdxA = alphabeticalOrder.indexOf(a[property][0])
+        let relativeIdxB = alphabeticalOrder.indexOf(b[property][0])
         equal = relativeIdxA === relativeIdxB
 
         let strChar = 0
@@ -86,16 +107,21 @@ const arbitraryKeySort = () => {
             strChar += 1
             if (!a.spelling.length >= strChar + 1) return 1
             if (!b.spelling.length >= strChar + 1) return -1
-            relativeIdxA = arbitrayKey.indexOf(a.spelling[strChar])
-            relativeIdxB = arbitrayKey.indexOf(b.spelling[strChar])
+            relativeIdxA = alphabeticalOrder.indexOf(a[property][strChar])
+            relativeIdxB = alphabeticalOrder.indexOf(b[property][strChar])
             equal = relativeIdxA === relativeIdxB
         }
         if (relativeIdxA > relativeIdxB) return 1
         else return -1
-    })
+    }
+
+
+    for (let sortMethod of sortTypes) {
+        const sortedLetters = sortWords(sortMethod)
+        console.log(sortedLetters)
+    }
+
 }
 
-for (let sortMethod of sortTypes) {
-    const sortedLetters = sortWords(sortMethod)
-    console.log(sortedLetters)
-}
+
+sort(inputAlphabeticalOrder)
